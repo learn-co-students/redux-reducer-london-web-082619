41db5aff72763ae3f6187dbafa7e0421890c76f8
Mike Thurgood <mikecthurgood@gmail.com>
2019-11-15 10:32:18 +0000

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,8 +1,11 @@
 export function managePresents(state = { numberOfPresents: 0 }, action) {
     switch (action.type) {
+
         case 'INCREASE':
             return { ...state, numberOfPresents: state.numberOfPresents + 1 }
+
         default:
             return state
+
     }
 }
